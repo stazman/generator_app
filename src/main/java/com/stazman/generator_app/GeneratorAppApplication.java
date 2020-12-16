9ef8df539b48b0bb207e822f927fbaa61aca3079
Christopher DiStasio <cadistasio@gmail.com>
2020-12-16 15:12:31 -0600

    Initial commit

---
+++ src/main/java/com/stazman/generator_app/GeneratorAppApplication.java
@@ -0,0 +1,13 @@
+package com.stazman.generator_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stazman/generator_app/GeneratorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stazman.generator_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
